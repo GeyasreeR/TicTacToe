837b130ea489728b3ddf5803af92e53da07aca1d
Geyasree <geyasree12@gmail.com>
2025-04-02 09:25:43 +0530

    Testing CICD for version 3

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html") + " - Version 0.3!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
